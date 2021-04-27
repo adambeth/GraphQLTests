0bc03896b47d95b7bbfaf769e1105f88117bfb00
Adam Bethlehem <adam.bethlehem1@gmail.com>
2021-04-27 12:21:52 +0200

    g

--- src/main/java/RequestSpecifications/Base.java
+++ src/main/java/RequestSpecifications/Base.java
@@ -5,11 +5,13 @@ https://www.javadoc.io/doc/io.rest-assured/rest-assured/latest/io/restassured/bu
 
 package RequestSpecifications;
 import Queries.GraphQLQuery;
+import com.google.common.collect.ImmutableMap;
 import io.qameta.allure.restassured.AllureRestAssured;
 import io.restassured.builder.RequestSpecBuilder;
 import io.restassured.http.ContentType;
 import io.restassured.specification.RequestSpecification;
 import org.testng.annotations.BeforeClass;
+import static com.github.automatedowl.tools.AllureEnvironmentWriter.allureEnvironmentWriter;
 
 public class Base {
 
@@ -29,5 +31,14 @@ public class Base {
                 .setBaseUri(url)
                 .build()
                 .filter(new AllureRestAssured());
+
+
+        allureEnvironmentWriter(
+                ImmutableMap.<String, String>builder()
+                        .put("Company", "ENVEL")
+                        .put("Server", "MockQL")
+                        .put("URL", "http://52.191.233.11:4000/")
+                        .build(), System.getProperty("user.dir")
+                        + "/allure-results/");
     }
 }
